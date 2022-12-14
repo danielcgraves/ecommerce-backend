cf7e642e40b0cb79c996adb3f8d8f2fb65b815ec
Daniel Graves <68198944+danielcgraves@users.noreply.github.com>
2022-12-14 16:33:55 -0700

    Filled in the rest of the codes for CRUD in teh product adn tag routes:

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -21,7 +21,7 @@ router.get('/:id', async (req, res) => {
   // be sure to include its associated Category and Tag data
   try {
     const productData = await Product.findByPk(req.params.id, {
-      include: [{ model: Tag, through: ProductTag, as: 'product_tags'}]
+      include: [{ model: Tag, through: ProductTag, as: 'product_tags' }]
     });
 
     if (!productData) {
@@ -115,8 +115,24 @@ router.put('/:id', (req, res) => {
     });
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete one product by its `id` value
+  try {
+    const productData = await Product.destroy({
+      where: {
+        id: req.params.id
+      }
+    });
+
+    if (!productData) {
+      res.status(404).json({ message: 'No product found with this id!' });
+      return;
+    }
+
+    res.status(200).json(productData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
