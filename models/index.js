e2155bcbb34471c4511790fdf2e824dd2cfab328
Daniel Graves <68198944+danielcgraves@users.noreply.github.com>
2022-12-14 12:56:24 -0700

    added information into models

--- models/index.js
+++ models/index.js
@@ -6,6 +6,8 @@ const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
 
+Product.belongsTo(Category)
+
 // Categories have many Products
 
 // Products belongToMany Tags (through ProductTag)
